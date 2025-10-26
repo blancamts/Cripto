7f135b98b3c29893f827e8a912b4ac508dd68a24
luisnf5 <luisnun.fdez@gmail.com>
2025-10-26 02:57:24 +0200

    fweg

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Usage: ./kasiski [-n ngram_length] [-i infile] [-o outfile]\n");
+    printf("Usage: ./subkeys [-n n] [-l language (0 for english / 1 for spanish)] [-i infile] [-o outfile]\n");
     return 0;
 }
