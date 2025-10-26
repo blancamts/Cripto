22b454fab4ec1c87d106d93d6e9db6bf67981468
luisnf5 <luisnun.fdez@gmail.com>
2025-10-26 19:50:03 +0100

    peref

--- IC.c
+++ IC.c
@@ -108,6 +108,7 @@ int main(int argc, char *argv[]) {
 
     for (i = 1; i < n; i++){
         double ic = calculate_ic(buffer, bytes_read, i);
+        
 
         if (fabs(ic - IC_ENGLISH) < fabs(best_ic - IC_ENGLISH)){
             best_ic = ic;
